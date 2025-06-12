d1f36a4d4bbf01902b5477508ae1a24a0ed7e055
Swastik Nandy <nswastik2003@gmail.com>
2025-06-13 02:18:46 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,19 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
+    <div
+      className="bg-[#dfcbb2]" 
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Navbar />
       <HeroSection />
       <OverviewSection />
       <RoomsPreviewSection />
       <Footer />
-    </>
+    </div>
   );
 }
