05c986d371442afe796dc53eca94d33524caa0db
Swastik Nandy <nswastik2003@gmail.com>
2025-06-15 07:59:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ ADD THIS
+import { BrowserRouter } from 'react-router-dom'; 
 import './index.css';
 import App from './App.jsx';
 import "aos/dist/aos.css";
@@ -10,7 +10,7 @@ AOS.init({ once: true, duration: 1000 });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ Wrap App with BrowserRouter */}
+    <BrowserRouter> {/* Wrap App with BrowserRouter */}
       <App />
     </BrowserRouter>
   </StrictMode>,
